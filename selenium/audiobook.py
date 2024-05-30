4e48320b0263b28cefe2a21949b2cbebe47a7530
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-05-30 14:16:24 +0530

    updated selenium code

--- selenium/audiobook.py
+++ selenium/audiobook.py
@@ -4,35 +4,50 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
+import time
 from selenium.webdriver.edge.options import Options
-
+'''
 options = Options()
 options.headless=True
 options.add_argument('window-size=1920x1080')
-
+'''
 website = "https://www.audible.com/search"
 path = r"D:\Web-Scrapping-Using-Python\selenium\msedgedriver.exe"
 
 service = Service(executable_path=path)
-driver = webdriver.Edge(service=service,options=options)
+driver = webdriver.Edge(service=service)
 driver.get(website)
-#driver.maximize_window()
+driver.maximize_window()
 
-wait = WebDriverWait(driver, 10)
-#container = driver.find_element(By.CLASS_NAME,"adbl-impression-container ")
-container = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "adbl-impression-container")))
-products = container.find_elements(By.XPATH,'./li')
+# page navigation
+pagenavigation = driver.find_element(By.XPATH,'//ul[contains(@class,"pagingElements")]')
+pages = pagenavigation.find_elements(By.TAG_NAME,'li')
+lastpage=int(pages[-2].text)
 
-book_title= []
-book_author = []
-book_length = []
+current_page = 1
+
+while current_page<=lastpage:
 
 
-for product in products:
-    book_title.append(product.find_element(By.XPATH,'.//h3[contains(@class,"bc-heading")]').text)
-    book_author.append(product.find_element(By.XPATH,'.//li[contains(@class,"authorLabel")]').text)
-    book_length.append(product.find_element(By.XPATH,'.//li[contains(@class,"runtimeLabel")]').text)
-    
+    time.sleep(2)
+    wait = WebDriverWait(driver, 10)
+    container = driver.find_element(By.CLASS_NAME,"adbl-impression-container ")
+    #container = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "adbl-impression-container")))
+    products = container.find_elements(By.XPATH,'./li')
+
+    book_title= []
+    book_author = []
+    book_length = []
+
+
+    for product in products:
+        book_title.append(product.find_element(By.XPATH,'.//h3[contains(@class,"bc-heading")]').text)
+        book_author.append(product.find_element(By.XPATH,'.//li[contains(@class,"authorLabel")]').text)
+        book_length.append(product.find_element(By.XPATH,'.//li[contains(@class,"runtimeLabel")]').text)
+
+    current_page=current_page+1
+    nextpage = driver.find_element(By.XPATH,'//span[contains(@class,"nextButton")]')
+    nextpage.click()   
 driver.quit()
 
 data = {"title":book_title,
